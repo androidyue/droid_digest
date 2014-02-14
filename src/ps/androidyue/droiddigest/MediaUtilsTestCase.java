7207985710f1e75e94e55293d9d13fd67173a9c4
Andrew Wallace <andrewallanwallace@gmail.com>
2014-02-14 14:07:03 +0800

    Add demo code

---
+++ src/ps/androidyue/droiddigest/MediaUtilsTestCase.java
@@ -0,0 +1,10 @@
+package ps.androidyue.droiddigest;
+
+import android.test.AndroidTestCase;
+
+public class MediaUtilsTestCase extends AndroidTestCase {
+	private static final String LOGTAG = "MediaUtilsTestCase";
+	
+	
+	
+}
